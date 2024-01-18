c308e0eb55d2594e94de3335d822945a54facf44
elmar <elmarcera@gmail.com>
2024-01-18 17:12:40 +0800

    Updating Recipe Servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -66,8 +66,16 @@ const controlPagination = function (goToPage) {
   // 1) Render new results
   resultsView.render(model.getSearchResultsPage(goToPage));
 
-  // 45) Render new pagination buttons
+  // 2) Render new pagination buttons
   paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
 };
 
 ///////////////////////////////////////////////////////////
@@ -76,6 +84,7 @@ const controlPagination = function (goToPage) {
 // SEE PDF LECTURE AND VIDEO
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
