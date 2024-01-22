72a76d932086bb0b23057569eda08a1ff6e512fc
elmar <elmarcera@gmail.com>
2024-01-22 11:53:29 +0800

    Implementing Bookmarks - Part 2

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable'; // polyfilling everything else.
 import 'regenerator-runtime'; // polyfilling async/await
@@ -28,6 +29,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) loading recipe
     await model.loadRecipe(id);
@@ -83,12 +85,16 @@ const controlServings = function (newServings) {
 };
 
 controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else if (model.state.recipe.bookmarked)
     model.deleteBookmark(model.state.recipe.id);
 
-  console.log(model.state.recipe);
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 ///////////////////////////////////////////////////////////
