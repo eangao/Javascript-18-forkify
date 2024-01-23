b5e48603770bd1f45e7d961c76d3e0493f7bad5e
elmar <elmarcera@gmail.com>
2024-01-23 14:57:40 +0800

    Wrapping Up: Final Considerations

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -21,6 +21,17 @@ export default class View {
 
   _data;
 
+  //https://jsdoc.app/
+
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View object
+   * @author Elmar Angao
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
